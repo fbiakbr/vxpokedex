8baba3d99f4d4ae2360d9f4880bb546f985c71cf
fbiakbr <febiakbar19@gmail.com>
2022-08-16 12:08:06 +0700

    remove pagination button

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -1,20 +1,6 @@
 <template>
     <div>
-        // make pagination button clickable
-        <button
-            class="btn btn-primary"
-            v-on:click="goToPage(page - 1)"
-            v-if="page > 1"
-        >
-            Previous
-        </button>
-        <button
-            class="btn btn-primary"
-            v-on:click="goToPage(page + 1)"
-            v-if="page < totalPages"
-        >
-            Next
-        </button>
+        
     </div>
 </template>
 
